6a0f320f70f191798f48ce789ecf57c4a65d0269
Bartłomiej Nowak <bartlomiej.nwk@gmail.com>
2021-10-07 22:38:18 +0200

    First iteration at alpha-beta-gamma filter

--- main.py
+++ main.py
@@ -28,28 +28,54 @@ class AlphaBetaFilter:
         self.est = state
 
     def estimate(self, m, dt):
-        # Predict next state using system's dynamic model
+        # Predict next state
         self.pred = [
             self.est[0] + dt * self.est[1], 
-            self.est[1]] # Assuming constant
+            self.est[1],]
 
         # Estimate current state
         self.est = [
             self.pred[0] + self.a * (m[0] - self.pred[0]),
-            self.pred[1] + self.b * ((m[0] - self.pred[0]) / dt)]
+            self.pred[1] + self.b * ((m[0] - self.pred[0]) / dt),]
 
         return self.est
 
 
+class AlphaBetaGammaFilter:
+    def __init__(self, a, b, g):
+        self.a = a
+        self.b = b
+        self.g = g
+
+    def initialize(self, state):
+        self.est = state
+
+    def estimate(self, m, dt):
+        # Predict next state
+        self.pred = [
+            self.est[0] + self.est[1] * dt + self.est[2] * (pow(dt, 2) / 2), 
+            self.est[1] + self.est[2] * dt,]
+
+        # Estimate current state
+        self.est = [
+            self.pred[0] + self.a * (m[0] - self.pred[0]),
+            self.pred[1] + self.b * ((m[0] - self.pred[0]) / dt),
+            self.pred[2] + self.g * ((m[0] - self.pred[0]) * (pow(dt, 2) / 2))]
+
+        return self.est
+
 def main():
-    filter = AlphaBetaFilter(0.2, 0.1)
+    filter = AlphaBetaGammaFilter(0.5, 0.4, 0.1)
 
-    logger.info('Initialize: 30000 40')
-    filter.initialize([30000, 40])
+    logger.info('Initialize: 30000 50 0')
+    filter.initialize([30000, 50, 0])
 
-    logger.info("New measurement: 30110")
-    val = filter.estimate([30110], 5)
+    logger.info("New measurement: 30160 at 5s interval")
+    val = filter.estimate([30160], 5)
+    logger.info(f"Prediction: {val}")
 
+    logger.info("New measurement: 30365 at 5s interval")
+    val = filter.estimate([30365], 5)
     logger.info(f"Prediction: {val}")
     
 
